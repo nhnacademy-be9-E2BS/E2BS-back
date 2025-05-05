551f298c7566a35b3e3656753b7f7c05aaaf6bac
K-Y-k <kyk4957@naver.com>
2025-05-05 16:55:41 +0900

    feat(entity): 회원 관련된 엔티티들 추가

--- src/main/java/com/nhnacademy/back/account/pointhistory/domain/entity/PointHistory.java
+++ src/main/java/com/nhnacademy/back/account/pointhistory/domain/entity/PointHistory.java
@@ -8,10 +8,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PointHistory {
 
 	@Id
