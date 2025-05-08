0c16dc806296895815331f9a79136e711475c71f
K-Y-k <kyk4957@naver.com>
2025-05-08 12:38:37 +0900

    modify(entity, sql): 주문 엔티티에 결제 상태 필드 추가 및 결제 엔티티와의 관계 매핑을 결제 엔티티가 주인으로 변경, 장바구니를 cart와 cartitems로 분리 및 sql 적용

--- src/main/resources/sql/data/cart.sql
+++ src/main/resources/sql/data/cart.sql
@@ -1,6 +1,12 @@
 -- 이 파일의 구문 실행 전에 account.sql, product.sql 실행하기
 
 -- Cart
-insert into cart (customer_id, product_id, cart_quantity)
-values (1, 1, 5),
-       (1, 2, 2);
+-- 비회원/회원용 장바구니
+INSERT INTO cart (cart_id, customer_id)
+VALUES (1, 1);
+
+-- CartItems
+INSERT INTO cart_items (cart_items_id, cart_id, product_id, cart_items_quantity)
+VALUES
+    (1, 1, 1, 1),
+    (2, 1, 2, 2);
