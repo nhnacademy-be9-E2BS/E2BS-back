c215d06c490a8c16b0260126f19b8c9a9f53943b
K-Y-k <kyk4957@naver.com>
2025-05-08 15:25:32 +0900

    feat(error): 공통 처리 에러 핸들러 적용

---
+++ src/main/java/com/nhnacademy/back/common/error/advice/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.nhnacademy.back.common.error.advice;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.nhnacademy.back.account.customer.exception.CustomerNotFoundException;
+import com.nhnacademy.back.cart.exception.CartItemAlreadyExistsException;
+import com.nhnacademy.back.cart.exception.CartItemNotFoundException;
+import com.nhnacademy.back.common.error.dto.GlobalErrorResponse;
+import com.nhnacademy.back.product.product.exception.ProductNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	/**
+	 * 이미 존재하는 경우의 에러 핸들러
+	 */
+	@ExceptionHandler({CartItemAlreadyExistsException.class})
+	public ResponseEntity<?> handleAlreadyExistsException(Exception ex) {
+		GlobalErrorResponse body = new GlobalErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value(), LocalDateTime.now());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+	}
+
+	/**
+	 * 찾지 못한 경우의 에러 핸들러
+	 */
+	@ExceptionHandler({CustomerNotFoundException.class, ProductNotFoundException.class, CartItemNotFoundException.class})
+	public ResponseEntity<?> handleNotFoundException(Exception ex) {
+		GlobalErrorResponse body = new GlobalErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value(), LocalDateTime.now());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+	}
+
+
+}
