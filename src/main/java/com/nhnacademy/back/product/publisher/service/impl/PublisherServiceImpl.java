0602be6b3b12e8723165685efd1223be1710ddc9
이영채 <dudco5858@naver.com>
2025-05-08 14:03:34 +0900

    modify(repository/service/test): publisher create 코드 수정 (출판사 이름 중복 허용 X)

--- src/main/java/com/nhnacademy/back/product/publisher/service/impl/PublisherServiceImpl.java
+++ src/main/java/com/nhnacademy/back/product/publisher/service/impl/PublisherServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.nhnacademy.back.product.publisher.domain.dto.request.RequestPublisherDTO;
 import com.nhnacademy.back.product.publisher.domain.entity.Publisher;
+import com.nhnacademy.back.product.publisher.exception.PublisherAlreadyExistsException;
 import com.nhnacademy.back.product.publisher.exception.PublisherNotFoundException;
 import com.nhnacademy.back.product.publisher.repository.PublisherJpaRepository;
 import com.nhnacademy.back.product.publisher.service.PublisherService;
@@ -21,10 +22,16 @@ public class PublisherServiceImpl implements PublisherService {
 
 	/**
 	 * Publisher을 DB에 저장하는 로직
+	 * publisher_name이 이미 존재하는 경우 Exception 발생
 	 */
 	@Override
 	public void createPublisher(RequestPublisherDTO request) {
-		Publisher publisher = new Publisher(request.getPublisherName());
+		String publisherName = request.getPublisherName();
+		if (publisherJpaRepository.existsByPublisherName(publisherName)) {
+			throw new PublisherAlreadyExistsException();
+		}
+
+		Publisher publisher = new Publisher(publisherName);
 		publisherJpaRepository.save(publisher);
 	}
 
