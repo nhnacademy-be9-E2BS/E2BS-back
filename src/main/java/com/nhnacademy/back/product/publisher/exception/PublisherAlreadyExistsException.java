2304101e5621cda33c1852fadad5221bf5004789
YoungChae01 <130735493+YoungChae01@users.noreply.github.com>
2025-05-09 13:38:10 +0900

    modify(Publisher): service, controller, exception, test 코드 수정 (#33)
    Publisher 코드 수정 (service, controller, exception, dto)
    
    수정 사항
    - publisher 리스트 조회 시 페이징 처리 -> 이에 따른 test도 수정
    - exception 메세지 추가
    - publisher update 시 출판사 이름 중복 검사 로직 추가 -> 이에 따른 test도 생성 및 수정
    - publisherJpaRepositoryTest 추가로 작성

--- src/main/java/com/nhnacademy/back/product/publisher/exception/PublisherAlreadyExistsException.java
+++ src/main/java/com/nhnacademy/back/product/publisher/exception/PublisherAlreadyExistsException.java
@@ -1,4 +1,7 @@
 package com.nhnacademy.back.product.publisher.exception;
 
 public class PublisherAlreadyExistsException extends RuntimeException {
+	public PublisherAlreadyExistsException(String message) {
+		super(message);
+	}
 }
--- src/test/java/com/nhnacademy/back/product/publisher/PublisherServiceTest.java
+++ src/test/java/com/nhnacademy/back/product/publisher/PublisherServiceTest.java
@@ -1,7 +1,6 @@
 package com.nhnacademy.back.product.publisher;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.util.List;
@@ -13,8 +12,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.nhnacademy.back.product.publisher.domain.dto.request.RequestPublisherDTO;
+import com.nhnacademy.back.product.publisher.domain.dto.response.ResponsePublisherDTO;
 import com.nhnacademy.back.product.publisher.domain.entity.Publisher;
 import com.nhnacademy.back.product.publisher.exception.PublisherAlreadyExistsException;
 import com.nhnacademy.back.product.publisher.exception.PublisherNotFoundException;
@@ -31,23 +35,29 @@ public class PublisherServiceTest {
 	@Test
 	@DisplayName("create publisher - success")
 	void create_publisher_success_test() {
+		// given
 		RequestPublisherDTO request = new RequestPublisherDTO("Publisher A");
+
+		// when
 		publisherService.createPublisher(request);
 
+		// then
 		verify(publisherJpaRepository, times(1)).save(any(Publisher.class));
 	}
 
 	@Test
 	@DisplayName("create publisher - fail")
 	void create_publisher_fail_test() {
+		// given
 		RequestPublisherDTO request = new RequestPublisherDTO("Publisher A");
-
 		when(publisherJpaRepository.existsByPublisherName("Publisher A"))
 			.thenReturn(false)    // 첫 번째 호출에는 false return
 			.thenReturn(true);    // 두 번째 호출에는 true return
 
+		// when
 		publisherService.createPublisher(request);
 
+		// then
 		assertThatThrownBy(() -> publisherService.createPublisher(request))
 			.isInstanceOf(PublisherAlreadyExistsException.class);
 	}
@@ -55,25 +65,37 @@ public class PublisherServiceTest {
 	@Test
 	@DisplayName("get publisher list")
 	void get_publishers_test() {
+		// given
 		Publisher publisherA = new Publisher("Publisher A");
 		Publisher publisherB = new Publisher("Publisher B");
 		List<Publisher> publishers = List.of(publisherA, publisherB);
 
-		when(publisherJpaRepository.findAll()).thenReturn(publishers);
-		List<Publisher> result = publisherService.getPublishers();
+		Pageable pageable = PageRequest.of(0, 10);
+		Page<Publisher> wrapperPage = new PageImpl<>(publishers);
 
-		assertEquals(2, result.size());
-		verify(publisherJpaRepository, times(1)).findAll();
+		when(publisherJpaRepository.findAll(pageable)).thenReturn(wrapperPage);
+
+		// when
+		Page<ResponsePublisherDTO> result = publisherService.getPublishers(pageable);
+
+		// then
+		assertThat(result.getContent()).hasSize(2);
+		assertThat(result.getContent().get(0).getPublisherName()).isEqualTo("Publisher A");
+		assertThat(result.getContent().get(1).getPublisherName()).isEqualTo("Publisher B");
 	}
 
 	@Test
 	@DisplayName("update publisher - success")
 	void update_publisher_success_test() {
+		// given
 		RequestPublisherDTO request = new RequestPublisherDTO("update after publisher");
 		Publisher publisher = new Publisher("update before publisher");
 		when(publisherJpaRepository.findById(1L)).thenReturn(Optional.of(publisher));
+
+		// when
 		publisherService.updatePublisher(1L, request);
 
+		// then
 		assertThat(publisher.getPublisherName()).isEqualTo("update after publisher");
 		verify(publisherJpaRepository, times(1)).save(publisher);
 	}
@@ -81,6 +103,7 @@ public class PublisherServiceTest {
 	@Test
 	@DisplayName("update publisher - fail1")
 	void update_publisher_fail1_test() {
+		// when & then
 		assertThatThrownBy(() -> publisherService.updatePublisher(1L, null))
 			.isInstanceOf(IllegalArgumentException.class);
 	}
@@ -88,10 +111,25 @@ public class PublisherServiceTest {
 	@Test
 	@DisplayName("update publisher - fail2")
 	void update_publisher_fail2_test() {
+		// given
 		RequestPublisherDTO request = new RequestPublisherDTO("update publisher");
 		when(publisherJpaRepository.findById(anyLong())).thenReturn(Optional.empty());
 
+		// when & then
 		assertThatThrownBy(() -> publisherService.updatePublisher(100L, request))
 			.isInstanceOf(PublisherNotFoundException.class);
+	}
+
+	@Test
+	@DisplayName("update publisher - fail3")
+	void update_publisher_fail3_test() {
+		// given
+		RequestPublisherDTO request = new RequestPublisherDTO("update publisher");
+		when(publisherJpaRepository.findById(anyLong())).thenReturn(Optional.of(new Publisher("update publisher")));
+		when(publisherJpaRepository.existsByPublisherName(any())).thenReturn(true);
+
+		// when & then
+		assertThatThrownBy(() -> publisherService.updatePublisher(100L, request))
+			.isInstanceOf(PublisherAlreadyExistsException.class);
 	}
 }
