f17f885bbbad5b155da1aa0cfad23fb462bf6ce0
이영채 <dudco5858@naver.com>
2025-05-09 13:12:20 +0900

    modify(Publisher): service, controller, exception, test 코드 수정

--- src/main/java/com/nhnacademy/back/product/publisher/service/PublisherService.java
+++ src/main/java/com/nhnacademy/back/product/publisher/service/PublisherService.java
@@ -1,14 +1,15 @@
 package com.nhnacademy.back.product.publisher.service;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.nhnacademy.back.product.publisher.domain.dto.request.RequestPublisherDTO;
-import com.nhnacademy.back.product.publisher.domain.entity.Publisher;
+import com.nhnacademy.back.product.publisher.domain.dto.response.ResponsePublisherDTO;
 
 public interface PublisherService {
 	void createPublisher(RequestPublisherDTO request);
 
-	List<Publisher> getPublishers();
+	Page<ResponsePublisherDTO> getPublishers(Pageable pageable);
 
 	void updatePublisher(long publisherId, RequestPublisherDTO request);
 }
