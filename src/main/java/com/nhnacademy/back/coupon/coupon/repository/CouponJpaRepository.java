5bc01ed983b3aa5c3aee466fdf97a97c9b8e722f
asdasd7722 <psjune9707@naver.com>
2025-05-12 21:55:19 +0900

    feat(coupon): 임시저장2

--- src/main/java/com/nhnacademy/back/coupon/coupon/repository/CouponJpaRepository.java
+++ src/main/java/com/nhnacademy/back/coupon/coupon/repository/CouponJpaRepository.java
@@ -1,8 +1,11 @@
 package com.nhnacademy.back.coupon.coupon.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nhnacademy.back.coupon.coupon.domain.entity.Coupon;
 
 public interface CouponJpaRepository extends JpaRepository<Coupon, Long> {
+	List<Coupon> findByCouponName(String couponName);
 }
--- src/main/java/com/nhnacademy/back/coupon/coupon/service/impl/CouponServiceImpl.java
+++ src/main/java/com/nhnacademy/back/coupon/coupon/service/impl/CouponServiceImpl.java
@@ -6,12 +6,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.nhnacademy.back.common.exception.BadRequestException;
 import com.nhnacademy.back.coupon.coupon.domain.dto.request.RequestCouponDTO;
 import com.nhnacademy.back.coupon.coupon.domain.dto.response.ResponseCouponDTO;
+import com.nhnacademy.back.coupon.coupon.domain.entity.CategoryCoupon;
+import com.nhnacademy.back.coupon.coupon.domain.entity.Coupon;
 import com.nhnacademy.back.coupon.coupon.repository.CategoryCouponJpaRepository;
 import com.nhnacademy.back.coupon.coupon.repository.CouponJpaRepository;
 import com.nhnacademy.back.coupon.coupon.repository.ProductCouponJpaRepository;
 import com.nhnacademy.back.coupon.coupon.service.CouponService;
+import com.nhnacademy.back.coupon.couponpolicy.domain.entity.CouponPolicy;
+import com.nhnacademy.back.coupon.couponpolicy.exception.CouponPolicyNotFoundException;
+import com.nhnacademy.back.coupon.couponpolicy.repository.CouponPolicyJpaRepository;
+import com.nhnacademy.back.product.category.domain.entity.Category;
+import com.nhnacademy.back.product.category.repository.CategoryJpaRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,9 +27,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CouponServiceImpl implements CouponService {
 
+	private CouponPolicyJpaRepository couponPolicyJpaRepository;
 	private CouponJpaRepository couponJpaRepository;
 	private CategoryCouponJpaRepository categoryCouponJpaRepository;
 	private ProductCouponJpaRepository productCouponJpaRepository;
+	private CategoryJpaRepository categoryJpaRepository;
 
 	/**
 	 * 관리자가 쿠폰을 생성
@@ -31,7 +41,22 @@ public class CouponServiceImpl implements CouponService {
 	@Override
 	public void createCoupon(RequestCouponDTO request) {
 		if(Objects.isNull(request)) {
+			throw new BadRequestException("쿠폰 생성 요청 DTO 를 받지 못했습니다.");
+		}
 
+		CouponPolicy couponPolicy = couponPolicyJpaRepository.findById(request.getCouponPolicyId())
+			.orElseThrow(()-> new CouponPolicyNotFoundException("존재하지 않는 쿠폰 정책입니다"));
+
+		Coupon coupon = new Coupon(couponPolicy, request.getCouponName());
+
+		// Coupon savedCoupon = couponJpaRepository.save(coupon);
+		// Long id = savedCoupon.getCouponId();
+
+		if(request.getCategoryId() != null) {
+			Category category = categoryJpaRepository.findById(request.getCategoryId())
+				.orElseThrow(); // todo CategoryNotFoundException 추가 예정
+			CategoryCoupon categoryCoupon = new CategoryCoupon(coupon, category);
+			categoryCouponJpaRepository.save(categoryCoupon);
 		}
 	}
 
