6426f1c8016c06611e9c1d85df142843a32c2591
YongKyeong Kim <102020649+K-Y-k@users.noreply.github.com>
2025-05-12 17:11:44 +0900

    Update RedisTestConfig.java

--- src/main/java/com/nhnacademy/back/common/config/RedisTestConfig.java
+++ src/main/java/com/nhnacademy/back/common/config/RedisTestConfig.java
@@ -1,26 +1,26 @@
-package com.nhnacademy.back.common.config;
+// package com.nhnacademy.back.common.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+// import org.springframework.data.redis.core.RedisTemplate;
 
-@Configuration
-public class RedisTestConfig {
+// @Configuration
+// public class RedisTestConfig {
 
-	@Bean
-	public RedisTemplate<String, Object> redisTemplate() {
-		RedisTemplate<String, Object> template = new RedisTemplate<>();
-		template.setConnectionFactory(jedisConnectionFactory());
-		return template;
-	}
+// 	@Bean
+// 	public RedisTemplate<String, Object> redisTemplate() {
+// 		RedisTemplate<String, Object> template = new RedisTemplate<>();
+// 		template.setConnectionFactory(jedisConnectionFactory());
+// 		return template;
+// 	}
 
-	@Bean
-	public JedisConnectionFactory jedisConnectionFactory() {
-		JedisConnectionFactory factory = new JedisConnectionFactory();
-		factory.setHostName("localhost");
-		factory.setPort(6379);
-		return factory;
-	}
+// 	@Bean
+// 	public JedisConnectionFactory jedisConnectionFactory() {
+// 		JedisConnectionFactory factory = new JedisConnectionFactory();
+// 		factory.setHostName("localhost");
+// 		factory.setPort(6379);
+// 		return factory;
+// 	}
 
-}
+// }
