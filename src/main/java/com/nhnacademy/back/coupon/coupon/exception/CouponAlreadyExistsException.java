5846fb8c194d055676179c12d21ba8669d4f30b6
asdasd7722 <psjune9707@naver.com>
2025-05-13 14:18:21 +0900

    feat(coupon): controller 테스트 구현

--- src/main/java/com/nhnacademy/back/coupon/coupon/exception/CouponAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.back.coupon.coupon.exception;
-
-public class CouponAlreadyExistsException extends RuntimeException {
-	public CouponAlreadyExistsException(String message) {
-		super(message);
-	}
-}
