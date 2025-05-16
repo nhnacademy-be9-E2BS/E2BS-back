e3e685a427590866e5d831c930eefcfd31cb363d
YoungChae01 <130735493+YoungChae01@users.noreply.github.com>
2025-05-16 14:22:37 +0900

    modify(product): product sonarqube 이슈 해결 (#68)
    product 소나큐브 이슈 해결

--- src/main/java/com/nhnacademy/back/product/contributor/domain/dto/request/RequestPositionDTO.java
+++ src/main/java/com/nhnacademy/back/product/contributor/domain/dto/request/RequestPositionDTO.java
@@ -1,7 +1,6 @@
 package com.nhnacademy.back.product.contributor.domain.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
