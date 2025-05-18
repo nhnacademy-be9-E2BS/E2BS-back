b7f69682edd55245772fff390e5a642872537eef
Kim-Yong-Kyeong <kyk4957@naver.com>
2025-05-19 00:01:16 +0900

    feat(review): 리뷰 관련 crud 구현중

--- src/main/java/com/nhnacademy/back/review/repository/ReviewJpaRepository.java
+++ src/main/java/com/nhnacademy/back/review/repository/ReviewJpaRepository.java
@@ -1,8 +1,13 @@
 package com.nhnacademy.back.review.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nhnacademy.back.review.domain.entity.Review;
 
 public interface ReviewJpaRepository extends JpaRepository<Review, Long> {
+	Page<Review> findAllByCustomer_CustomerId(long customerCustomerId, Pageable pageable);
+
+	Page<Review> findAllByProduct_ProductId(long productProductId, Pageable pageable);
 }
---
+++ src/main/java/com/nhnacademy/back/review/service/ReviewService.java
@@ -0,0 +1,16 @@
+package com.nhnacademy.back.review.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.nhnacademy.back.review.domain.dto.request.RequestCreateReviewDTO;
+import com.nhnacademy.back.review.domain.dto.request.RequestUpdateReviewDTO;
+import com.nhnacademy.back.review.domain.dto.response.ResponseReviewPageDTO;
+
+public interface ReviewService {
+	void createReview(RequestCreateReviewDTO request);
+	void updateReview(long reviewId, RequestUpdateReviewDTO request);
+	void deleteReview(long reviewId);
+	Page<ResponseReviewPageDTO> getReviewsByCustomer(long customerId, Pageable pageable);
+	Page<ResponseReviewPageDTO> getReviewsByProduct(long productId, Pageable pageable);
+}
