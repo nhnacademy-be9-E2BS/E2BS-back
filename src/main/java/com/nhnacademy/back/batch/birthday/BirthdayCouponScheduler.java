f3b8af9c8824837c6b21f6768c82333fdf3784cf
asdasd7722 <psjune9707@naver.com>
2025-05-21 15:45:11 +0900

    feat(Coupon): 생일, 웰컴, 관리자 발급 쿠폰 Batch, RabbitMQ 적용
    - 관리자 발급 : Batch
    - 생일 : Batch + RabbitMQ
    - 웰컴 : RabbitMQ

--- src/main/java/com/nhnacademy/back/batch/birthday/BirthdayCouponScheduler.java
+++ src/main/java/com/nhnacademy/back/batch/birthday/BirthdayCouponScheduler.java
@@ -15,7 +15,7 @@ public class BirthdayCouponScheduler {
 	private final JobLauncher jobLauncher;
 	private final Job birthdayCouponJob;
 
-	@Scheduled(cron = "0 * * * * *") // 테스트용: 매분 -> 실서비스 : 매월 1일 00시 (cron = "0 0 0 1 * *")
+	@Scheduled(cron = "0 0 * * * *") // 테스트용: 매분 -> 실서비스 : 매월 1일 00시 (cron = "0 0 0 1 * *")
 	public void runBirthdayCouponJob() throws Exception {
 		JobParameters params = new JobParametersBuilder()
 			.addLong("time", System.currentTimeMillis()) // 중복 실행 방지
