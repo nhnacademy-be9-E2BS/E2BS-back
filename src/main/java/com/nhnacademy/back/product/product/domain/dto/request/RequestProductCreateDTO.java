1002ead750eb14d01d12470516897a1ed901deff
김현규 <khk15888@naver.com>
2025-05-22 12:16:50 +0900

    product 수정 전 저장(name -> id)

--- src/main/java/com/nhnacademy/back/product/product/domain/dto/request/RequestProductCreateDTO.java
+++ src/main/java/com/nhnacademy/back/product/product/domain/dto/request/RequestProductCreateDTO.java
@@ -18,7 +18,7 @@ public class RequestProductCreateDTO {
 	 */
 
 	//출판사
-	private String publisherName;
+	private long publisherId;
 
 	//제목, 목차, 설명
 	@NotNull
@@ -42,13 +42,13 @@ public class RequestProductCreateDTO {
 	// 이미지
 	private List<String> productImagePaths;
 	// 태그
-	private List<String> tagNames;
+	private List<Long> tagIds;
 	// 카테고리 Id
 	private List<Long> categoryIds;
 	// 기여자 Id
-	private List<String> contributorNames;
+	private List<String> contributorIds;
 	// 기여자 역할
-	private List<String> positionNames; // 새로 추가
+	private List<String> positionIds; // 새로 추가
 
 
 }
