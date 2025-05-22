bdfa0cfc1e31d8912f4c0b7e9b8277a5076e6fa9
doyun <90900630+Doyun-coding@users.noreply.github.com>
2025-05-22 14:57:06 +0900

    feat(AddressController/AddressSaveController): 마이페이지 배송지 관리 기능 구현

---
+++ src/main/java/com/nhnacademy/back/account/address/exception/UpdateAddressFailedException.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.back.account.address.exception;
+
+public class UpdateAddressFailedException extends RuntimeException {
+	public UpdateAddressFailedException(String message) {
+		super(message);
+	}
+}
