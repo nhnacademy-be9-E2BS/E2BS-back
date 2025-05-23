b365f3f5b39844dc6bc0433087ac5d787621ce57
doyun <90900630+Doyun-coding@users.noreply.github.com>
2025-05-23 16:44:48 +0900

    modify(sonarqube): sonarqube를 위한 이슈 해결

--- src/main/java/com/nhnacademy/back/account/address/repository/AddressJpaRepository.java
+++ src/main/java/com/nhnacademy/back/account/address/repository/AddressJpaRepository.java
@@ -1,12 +1,13 @@
 package com.nhnacademy.back.account.address.repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.nhnacademy.back.account.address.domain.dto.UpdateAddressDTO;
 import com.nhnacademy.back.account.address.domain.entity.Address;
 
 public interface AddressJpaRepository extends JpaRepository<Address, Long> {
@@ -16,11 +17,10 @@ public interface AddressJpaRepository extends JpaRepository<Address, Long> {
 	Address getAddressByAddressId(long addressId);
 
 	@Modifying(clearAutomatically = true)
-	@Query("UPDATE Address a SET a.addressAlias = :addressAlias, a.addressCode = :addressCode, a.addressInfo = :addressInfo, a.addressDetail = :addressDetail, a.addressExtra = :addressExtra, a.addressDefault = :addressDefault, a.addressCreatedAt = :addressCreatedAt WHERE a.addressId = :addressId")
-	int updateAddress(
-		String addressAlias, String addressCode, String addressInfo,
-		String addressDetail, String addressExtra, boolean addressDefault,
-		LocalDateTime addressCreatedAt, long addressId
-	);
+	@Query("UPDATE Address a SET a.addressAlias = :#{#dto.addressAlias}, a.addressCode = :#{#dto.addressCode}, " +
+		"a.addressInfo = :#{#dto.addressInfo}, a.addressDetail = :#{#dto.addressDetail}, " +
+		"a.addressExtra = :#{#dto.addressExtra}, a.addressDefault = :#{#dto.addressDefault}, " +
+		"a.addressCreatedAt = :#{#dto.addressCreatedAt} WHERE a.addressId = :#{#dto.addressId}")
+	int updateAddress(@Param("dto") UpdateAddressDTO dto);
 
 }
