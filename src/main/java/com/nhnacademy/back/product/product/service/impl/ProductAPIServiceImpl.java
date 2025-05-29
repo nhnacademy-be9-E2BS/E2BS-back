27c19aab95f294c8888a14c8384a79f65cdc20c6
Suin Park <tndls2490@o.cnu.ac.kr>
2025-05-29 15:36:31 +0900

    진행중

--- src/main/java/com/nhnacademy/back/product/product/service/impl/ProductAPIServiceImpl.java
+++ src/main/java/com/nhnacademy/back/product/product/service/impl/ProductAPIServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 import org.springframework.data.domain.Page;
