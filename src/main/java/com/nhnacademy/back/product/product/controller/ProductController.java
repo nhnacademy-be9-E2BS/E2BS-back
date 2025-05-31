2adde20c5112991fee2c2fd7f1ed7a92eaf71405
JS01C <cjs010131@gmail.com>
2025-05-31 17:20:33 +0900

    feat(Order): 주문서 페이지를 위한 수정

--- src/main/java/com/nhnacademy/back/product/product/controller/ProductController.java
+++ src/main/java/com/nhnacademy/back/product/product/controller/ProductController.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nhnacademy.back.product.product.domain.dto.response.ResponseProductReadDTO;
@@ -49,7 +49,7 @@ public class ProductController {
 	 * 200 상태코드 반환
 	 */
 	@GetMapping("/order")
-	public ResponseEntity<List<ResponseProductReadDTO>> getProducts(@RequestBody List<Long> products) {
+	public ResponseEntity<List<ResponseProductReadDTO>> getProducts(@RequestParam("products") List<Long> products) {
 		List<ResponseProductReadDTO> productsDTO = productService.getProducts(products);
 		return ResponseEntity.status(HttpStatus.OK).body(productsDTO);
 	}
