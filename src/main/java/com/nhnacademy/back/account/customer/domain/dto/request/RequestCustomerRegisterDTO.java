8925c061c7c97b98bccee2998b9dfd325a329f26
Kim-Yong-Kyeong <kyk4957@naver.com>
2025-06-01 18:10:02 +0900

    feat(cart): 비회원이 장바구니를 담은 경우의 주문시 선택된 장바구니를 세션에 담고 비회원 주문 페이지로 이동 및 가입, 로그인 적용 완료

---
+++ src/main/java/com/nhnacademy/back/account/customer/domain/dto/request/RequestCustomerRegisterDTO.java
@@ -0,0 +1,23 @@
+package com.nhnacademy.back.account.customer.domain.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestCustomerRegisterDTO {
+
+	@NotBlank
+	private String name;
+
+	@Email
+	private String email;
+
+	@NotBlank
+	private String password;
+
+}
