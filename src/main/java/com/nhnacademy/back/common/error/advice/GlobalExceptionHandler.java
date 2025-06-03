dfcc4d1f401bd9ec3ab95ab47fb2e8ae31091d92
JongSeong Choi <cjs010131@gmail.com>
2025-06-03 17:01:48 +0900

    Feature/order return 반품 기능 구현 (#142)
    * feat(Order): 반품 기능, 반품 조회(회원, 관리자) 기능 추가, 반품 엔티티 필드 수정
    
    * feat(Order): 테스트 수정

--- src/main/java/com/nhnacademy/back/common/error/advice/GlobalExceptionHandler.java
+++ src/main/java/com/nhnacademy/back/common/error/advice/GlobalExceptionHandler.java
@@ -33,6 +33,8 @@ import com.nhnacademy.back.common.exception.BadRequestException;
 import com.nhnacademy.back.common.exception.InvalidImageFormatException;
 import com.nhnacademy.back.common.exception.ValidationFailedException;
 import com.nhnacademy.back.order.order.exception.OrderDetailNotFoundException;
+import com.nhnacademy.back.order.order.exception.OrderNotFoundException;
+import com.nhnacademy.back.order.order.exception.OrderProcessException;
 import com.nhnacademy.back.order.wrapper.exception.WrapperNotFoundException;
 import com.nhnacademy.back.product.category.exception.CategoryAlreadyExistsException;
 import com.nhnacademy.back.product.category.exception.CategoryDeleteNotAllowedException;
@@ -66,7 +68,7 @@ public class GlobalExceptionHandler {
 		DeleteMemberFailedException.class, SaveAddressFailedException.class, UpdateAddressFailedException.class,
 		DeleteAddressFailedException.class,
 		ReviewAlreadyExistsException.class, RegisterOAuthFailedException.class, CustomerEmailNotExistsException.class,
-		CustomerEmailNotExistsException.class, CustomerEmailAlreadyExistsException.class})
+		CustomerEmailNotExistsException.class, CustomerEmailAlreadyExistsException.class, OrderProcessException.class})
 	public ResponseEntity<GlobalErrorResponse> handleAlreadyExistsException(Exception ex) {
 		GlobalErrorResponse body = new GlobalErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value(),
 			LocalDateTime.now());
@@ -82,7 +84,8 @@ public class GlobalExceptionHandler {
 		ReviewNotFoundException.class, TagNotFoundException.class,
 		NotFoundMemberException.class, PublisherNotFoundException.class, WrapperNotFoundException.class,
 		ReviewNotFoundException.class, MemberRoleException.class, CategoryNotFoundException.class,
-		NotFoundAddressException.class, OrderDetailNotFoundException.class, NotFoundMemberStateException.class})
+		NotFoundAddressException.class, OrderDetailNotFoundException.class, NotFoundMemberStateException.class,
+		OrderNotFoundException.class})
 	public ResponseEntity<GlobalErrorResponse> handleNotFoundException(Exception ex) {
 		GlobalErrorResponse body = new GlobalErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value(),
 			LocalDateTime.now());
--- src/main/java/com/nhnacademy/back/order/orderreturn/repository/OrderReturnJpaRepository.java
+++ src/main/java/com/nhnacademy/back/order/orderreturn/repository/OrderReturnJpaRepository.java
@@ -1,8 +1,16 @@
 package com.nhnacademy.back.order.orderreturn.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.nhnacademy.back.account.customer.domain.entity.Customer;
 import com.nhnacademy.back.order.orderreturn.domain.entity.OrderReturn;
 
-public interface OrderReturnJpaRepository extends JpaRepository<OrderReturn,Long> {
+public interface OrderReturnJpaRepository extends JpaRepository<OrderReturn, Long> {
+	Page<OrderReturn> findByOrder_CustomerOrderByOrderReturnCreatedAtDesc(Customer customer, Pageable pageable);
+	Optional<OrderReturn> findByOrder_OrderCode(String orderCode);
+	Page<OrderReturn> findAllByOrderByOrderReturnCreatedAtDesc(Pageable pageable);
 }
