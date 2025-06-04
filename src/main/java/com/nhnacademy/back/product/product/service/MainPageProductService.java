82305eca68e4102e775e39390f247a05f511be93
sooooooing <126747506+sooooooing@users.noreply.github.com>
2025-06-04 18:21:21 +0900

    Feature/product main (#149)
    * modify(product-api) : 헤더 카테고리별 페이지 이동
    
    * 진행중
    
    * 진행중
    
    * feature(product) : 베스트 상위카테고리 추가
    
    * modify(product) : 메인 페이지에 출판사, 상품 상세 설명 가져오기

--- src/main/java/com/nhnacademy/back/product/product/service/MainPageProductService.java
+++ src/main/java/com/nhnacademy/back/product/product/service/MainPageProductService.java
@@ -4,15 +4,15 @@ import java.util.List;
 
 import org.springframework.transaction.annotation.Transactional;
 
-import com.nhnacademy.back.product.product.domain.dto.request.RequestMainPageProductDTO;
 import com.nhnacademy.back.product.product.domain.dto.response.ResponseMainPageProductDTO;
 
 public interface MainPageProductService {
 	//메인 페이지에서 책 보여주기
-	List<ResponseMainPageProductDTO> getProducts(RequestMainPageProductDTO request);
 
 	@Transactional(readOnly = true)
+
 	List<ResponseMainPageProductDTO> getProductsByCategory(String categoryName);
+
 
 	List<ResponseMainPageProductDTO> getBestSellerProducts();
 
@@ -21,4 +21,6 @@ public interface MainPageProductService {
 	List<ResponseMainPageProductDTO> getNewItemsProducts();
 
 	List<ResponseMainPageProductDTO> getItemNewSpecialProducts();
+
+	List<ResponseMainPageProductDTO> getItemEditorChoiceProducts();
 }
