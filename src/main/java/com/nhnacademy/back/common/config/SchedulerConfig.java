086cd054dacb9131fa93dba3a491e6c87ac1755c
asdasd7722 <psjune9707@naver.com>
2025-06-05 17:29:46 +0900

    feat(scheduler): 생일쿠폰 중복 발급 문제 ShedLock으로 해결 시도

--- src/main/java/com/nhnacademy/back/common/config/SchedulerConfig.java
+++ src/main/java/com/nhnacademy/back/common/config/SchedulerConfig.java
@@ -1,4 +1,21 @@
 package com.nhnacademy.back.common.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+
 public class SchedulerConfig {
+	@Bean
+	public LockProvider lockProvider(DataSource dataSource) {
+		return new JdbcTemplateLockProvider(
+			JdbcTemplateLockProvider.Configuration.builder()
+				.withJdbcTemplate(new JdbcTemplate(dataSource))
+				.usingDbTime() // DB 시간 기준으로 잠금 시간 계산
+				.build()
+		);
+	}
 }
