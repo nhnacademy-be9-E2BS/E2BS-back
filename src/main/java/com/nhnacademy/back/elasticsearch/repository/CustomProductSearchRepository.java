f43aed72810d3f2ba94507d99a3ba576619a09e5
이영채 <dudco5858@naver.com>
2025-06-06 17:51:27 +0900

    feat(product): 베스트, 신상 도서 조회 기능 추가 (엘라스틱 서치)

--- src/main/java/com/nhnacademy/back/elasticsearch/repository/CustomProductSearchRepository.java
+++ src/main/java/com/nhnacademy/back/elasticsearch/repository/CustomProductSearchRepository.java
@@ -1,5 +1,7 @@
 package com.nhnacademy.back.elasticsearch.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -9,4 +11,12 @@ public interface CustomProductSearchRepository {
 	Page<Long> searchAndSortProductIds(Pageable pageable, String keyword, ProductSortType sortType);
 
 	Page<Long> categoryAndSortProductIds(Pageable pageable, Long categoryId, ProductSortType sortType);
+
+	List<Long> bestSellerProductIdsInMain();
+
+	List<Long> newestSellerProductIdsInMain();
+
+	Page<Long> bestSellerProductIds(Pageable pageable);
+
+	Page<Long> newestSellerProductIds(Pageable pageable);
 }
