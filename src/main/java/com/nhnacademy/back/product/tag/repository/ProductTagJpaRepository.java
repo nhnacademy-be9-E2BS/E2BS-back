61a263151ee5bc8921fcdcbbd533d70a3a9547ea
김현규 <khk15888@naver.com>
2025-06-12 15:47:11 +0900

    productState 테스트 코드 추가

--- src/main/java/com/nhnacademy/back/product/tag/repository/ProductTagJpaRepository.java
+++ src/main/java/com/nhnacademy/back/product/tag/repository/ProductTagJpaRepository.java
@@ -1,8 +1,6 @@
 package com.nhnacademy.back.product.tag.repository;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,7 +8,6 @@ import org.springframework.data.repository.query.Param;
 
 import com.nhnacademy.back.product.tag.domain.dto.response.ResponseTagDTO;
 import com.nhnacademy.back.product.tag.domain.entity.ProductTag;
-import com.nhnacademy.back.product.tag.domain.entity.Tag;
 
 public interface ProductTagJpaRepository extends JpaRepository<ProductTag, Long> {
 	@Query("select new com.nhnacademy.back.product.tag.domain.dto.response.ResponseTagDTO(pt.tag.tagId, pt.tag.tagName) from ProductTag pt where pt.product.productId = :productId")
@@ -20,14 +17,6 @@ public interface ProductTagJpaRepository extends JpaRepository<ProductTag, Long>
 
 	@Query("SELECT pt FROM ProductTag pt JOIN FETCH pt.tag WHERE pt.product.productId IN :productIds")
 	List<ProductTag> findAllWithTagsByProductIds(@Param("productIds") List<Long> productIds);
-
-	default Map<Long, List<Tag>> findTagsGroupedByProductIds(List<Long> productIds) {
-		return findAllWithTagsByProductIds(productIds).stream()
-			.collect(Collectors.groupingBy(
-				pt -> pt.getProduct().getProductId(),
-				Collectors.mapping(ProductTag::getTag, Collectors.toList())
-			));
-	}
 
 	List<ProductTag> findAllByTag_TagId(long tagTagId);
 }
